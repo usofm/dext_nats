41a9db3a5d3e3f0d68699ab4af8b72e72334d8eb
usofm <71825788+usofm@users.noreply.github.com>
2026-08-13 01:51:49 +0400

    Add parser v2 Delphi build switch

--- scripts/build-tests.ps1
+++ scripts/build-tests.ps1
@@ -5,7 +5,9 @@ param(
   [ValidateSet('Win32', 'Win64')]
   [string]$Platform = 'Win32',
 
-  [string]$BdsRoot = ''
+  [string]$BdsRoot = '',
+
+  [switch]$ParserV2
 )
 
 $ErrorActionPreference = 'Stop'
@@ -41,9 +43,16 @@ function Find-RsVars {
 $rsvars = Find-RsVars -ExplicitRoot $BdsRoot
 Write-Host "Using RAD Studio environment: $rsvars"
 Write-Host "Building: $project"
-Write-Host "Config=$Config Platform=$Platform"
+Write-Host "Config=$Config Platform=$Platform ParserV2=$($ParserV2.IsPresent)"
 
-$command = 'call "' + $rsvars + '" && msbuild "' + $project + '" /t:Build /p:Config=' + $Config + ' /p:Platform=' + $Platform
+$properties = '/p:Config=' + $Config + ' /p:Platform=' + $Platform
+if ($ParserV2) {
+  # Global MSBuild property is intentional for this validation build. It selects
+  # Dext.Net.Nats.Internal.ParserSelector without changing source files.
+  $properties += ' /p:DCC_Define=DEXT_NATS_PARSER_V2'
+}
+
+$command = 'call "' + $rsvars + '" && msbuild "' + $project + '" /t:Build ' + $properties
 & cmd.exe /d /s /c $command
 if ($LASTEXITCODE -ne 0) {
   throw "Delphi build failed with exit code $LASTEXITCODE"
