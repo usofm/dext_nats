f756366a0b6ffea482465e51026539213475c24e
usofm <71825788+usofm@users.noreply.github.com>
2026-08-13 01:44:09 +0400

    Register architecture v2 units and focused tests

--- Tests/Dext.Net.Nats.Tests.dpr
+++ Tests/Dext.Net.Nats.Tests.dpr
@@ -1,10 +1,3 @@
-﻿{***************************************************************************}
-{                                                                           }
-{           Dext.Nats                                                     }
-{                                                                           }
-{           A native NATS client library for the Dext Framework            }
-{                                                                           }
-{***************************************************************************}
 program DextNetNatsTests;
 
 {$APPTYPE CONSOLE}
@@ -17,6 +10,8 @@ uses
   Dext.Testing.Fluent,
   Dext.Utils,
   Dext.Net.Nats.Protocol in '..\Source\Dext.Net.Nats.Protocol.pas',
+  Dext.Net.Nats.Protocol.Headers in '..\Source\Protocol\Dext.Net.Nats.Protocol.Headers.pas',
+  Dext.Net.Nats.Protocol.Control in '..\Source\Protocol\Dext.Net.Nats.Protocol.Control.pas',
   Dext.Net.Nats.NKeys in '..\Source\Dext.Net.Nats.NKeys.pas',
   Dext.Net.Nats in '..\Source\Dext.Net.Nats.pas',
   Dext.Net.Nats.Dispatching in '..\Source\Dext.Net.Nats.Dispatching.pas',
@@ -25,12 +20,20 @@ uses
   Dext.Net.Nats.JetStream.Codecs in '..\Source\JetStream\Dext.Net.Nats.JetStream.Codecs.pas',
   Dext.Net.Nats.JetStream.Parsers in '..\Source\JetStream\Dext.Net.Nats.JetStream.Parsers.pas',
   Dext.Net.Nats.JetStream.Paging in '..\Source\JetStream\Dext.Net.Nats.JetStream.Paging.pas',
+  Dext.Net.Nats.JetStream.ObjectPaging in '..\Source\JetStream\Dext.Net.Nats.JetStream.ObjectPaging.pas',
   Dext.Net.Nats.JetStream.Transport in '..\Source\JetStream\Dext.Net.Nats.JetStream.Transport.pas',
   Dext.Net.Nats.JetStream.Streams in '..\Source\JetStream\Dext.Net.Nats.JetStream.Streams.pas',
   Dext.Net.Nats.JetStream.Consumers in '..\Source\JetStream\Dext.Net.Nats.JetStream.Consumers.pas',
+  Dext.Net.Nats.JetStream.Fetch in '..\Source\JetStream\Dext.Net.Nats.JetStream.Fetch.pas',
+  Dext.Net.Nats.JetStream.Push in '..\Source\JetStream\Dext.Net.Nats.JetStream.Push.pas',
+  Dext.Net.Nats.JetStream.Ordered in '..\Source\JetStream\Dext.Net.Nats.JetStream.Ordered.pas',
   Dext.Net.Nats.KeyValue in '..\Source\Dext.Net.Nats.KeyValue.pas',
+  Dext.Net.Nats.KeyValue.Subjects in '..\Source\KeyValue\Dext.Net.Nats.KeyValue.Subjects.pas',
   Dext.Net.Nats.ObjectStore in '..\Source\Dext.Net.Nats.ObjectStore.pas',
+  Dext.Net.Nats.ObjectStore.Subjects in '..\Source\ObjectStore\Dext.Net.Nats.ObjectStore.Subjects.pas',
+  Dext.Net.Nats.ObjectStore.Crypto in '..\Source\ObjectStore\Dext.Net.Nats.ObjectStore.Crypto.pas',
   Dext.Net.Nats.Services in '..\Source\Dext.Net.Nats.Services.pas',
+  Dext.Net.Nats.Services.Subjects in '..\Source\Services\Dext.Net.Nats.Services.Subjects.pas',
   Dext.Net.Nats.DependencyInjection in '..\Source\Dext.Net.Nats.DependencyInjection.pas',
   Dext.Net.Nats.HealthChecks in '..\Source\Dext.Net.Nats.HealthChecks.pas',
   Dext.Net.Nats.Internal.Buffer in '..\Source\Internal\Dext.Net.Nats.Internal.Buffer.pas',
@@ -41,10 +44,16 @@ uses
   Dext.Net.Nats.Dispatching.Tests in 'Core\Dext.Net.Nats.Dispatching.Tests.pas',
   Dext.Net.Nats.Internal.Tests in 'Internal\Dext.Net.Nats.Internal.Tests.pas',
   Dext.Net.Nats.ParserV2.Tests in 'Protocol\Dext.Net.Nats.ParserV2.Tests.pas',
+  Dext.Net.Nats.Protocol.V2.Tests in 'Protocol\Dext.Net.Nats.Protocol.V2.Tests.pas',
   Dext.Net.Nats.ParserV2.Benchmarks in 'Benchmarks\Dext.Net.Nats.ParserV2.Benchmarks.pas',
   Dext.Net.Nats.JetStream.Json.Tests in 'JetStream\Dext.Net.Nats.JetStream.Json.Tests.pas',
   Dext.Net.Nats.JetStream.Streams.Tests in 'JetStream\Dext.Net.Nats.JetStream.Streams.Tests.pas',
-  Dext.Net.Nats.JetStream.Consumers.Tests in 'JetStream\Dext.Net.Nats.JetStream.Consumers.Tests.pas';
+  Dext.Net.Nats.JetStream.Consumers.Tests in 'JetStream\Dext.Net.Nats.JetStream.Consumers.Tests.pas',
+  Dext.Net.Nats.JetStream.ObjectPaging.Tests in 'JetStream\Dext.Net.Nats.JetStream.ObjectPaging.Tests.pas',
+  Dext.Net.Nats.JetStream.Fetch.Tests in 'JetStream\Dext.Net.Nats.JetStream.Fetch.Tests.pas',
+  Dext.Net.Nats.ObjectStore.Subjects.Tests in 'ObjectStore\Dext.Net.Nats.ObjectStore.Subjects.Tests.pas',
+  Dext.Net.Nats.KeyValue.Subjects.Tests in 'KeyValue\Dext.Net.Nats.KeyValue.Subjects.Tests.pas',
+  Dext.Net.Nats.Services.Subjects.Tests in 'Services\Dext.Net.Nats.Services.Subjects.Tests.pas';
 
 var
   Config: TTestConfigurator;
@@ -82,10 +91,16 @@ begin
       TDextNatsInternalTests,
       TDextNatsDispatchingTests,
       TDextNatsParserV2Tests,
+      TDextNatsProtocolV2Tests,
       TDextNatsParserV2BenchmarkTests,
       TDextNatsJetStreamJsonTests,
       TDextNatsJetStreamStreamsTests,
-      TDextNatsJetStreamConsumersTests
+      TDextNatsJetStreamConsumersTests,
+      TDextNatsJetStreamObjectPagingTests,
+      TDextNatsJetStreamFetchTests,
+      TDextNatsObjectStoreSubjectsTests,
+      TDextNatsKeyValueSubjectsTests,
+      TDextNatsServicesSubjectsTests
     ]));
   except
     on E: Exception do
